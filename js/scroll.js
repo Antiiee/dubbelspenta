00537b4e592d67c3aa072332c05594c0d29f8ddd
danielronnkvist <daniel@ronnkvist.com>
2014-09-27 16:43:07 +0200

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+// This is a functions that scrolls to #{blah}link
+function goToByScroll(id){
+      // Remove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top},
+        'fast', function(){
+          $('#navbar-collapse-target').collapse()
+          console.log("TJENA")
+        });
+}
+
+$(".navbar-right > li > a").click(function(e) { 
+      // Prevent a page reload when a link is pressed
+    e.preventDefault(); 
+      // Call the scroll function
+    goToByScroll($(this).attr("id"));           
+});

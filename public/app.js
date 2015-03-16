acab2bec728c56808cc0b9b4036e81217010b603
danielronnkvist <daniel@ronnkvist.com>
2015-03-16 20:45:36 +0100

    Http.jsonp for cross domain

--- public/app.js
+++ public/app.js
@@ -4,14 +4,9 @@ app.controller('photos', ['$scope', '$http', function($scope, $http){
   var socket = io();
   socket.on('image', function(image){
     console.log(image);
-    $http({
-            url: image,
-            method: 'POST',
-            crossDomain: true,
-            dataType: 'jsonp'
-        }).success(function(response){
-          console.log(response);
-          $scope.image_url = image;
-        })
+    $http.jsonp(image, {'callback': 'JSON_CALLBACK'}).success(function(response){
+      console.log(response);
+      $scope.image_url = image;
+    })
   });
 }]);

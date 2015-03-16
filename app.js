11a2c13f7fbcc2e2c8a1889639b5b1fecc7f454f
danielronnkvist <daniel@ronnkvist.com>
2015-03-16 14:38:21 +0100

    Heroku modifications

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ var ig = require('instagram-node').instagram(),
     fs = require('fs'),
     express = require('express'),
     app = express(),
+    http = require('http'),
     io = require('socket.io')(http);
 
 // read instagram info
 var instaConf = JSON.parse(fs.readFileSync('instagram.json', 'UTF-8'));
-ig.use({ client_id: instaConf.APP_ID,
-         client_secret: instaConf.APP_SECRET });
+ig.use({ client_id: process.env.APP_ID,
+         client_secret: process.env.APP_SECRET });
 
 // EXPRESS CONFIG
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8000);
-app.use("/scripts",express.static(__dirname+"/scripts"));
-
-var server = app.listen(app.get('port'), function() {
-  console.log("Listening on port %d", server.address().port);
-});
+app.use("/public",express.static(__dirname+"/public"));
 
 // Subscribe to hashtag
-ig.add_tag_subscription('dubbelspenta', 'http://213.114.76.130/new_img', function(err, result, remaining, limit){
+ig.add_tag_subscription('dubbelspenta', 'http://dubbelspenta.herokuapp.com/new_img', function(err, result, remaining, limit){
   console.log("Successfully subscribed to hashtag");
 });
 
 // Bind all types of requests to '/new_img'
 app.all('/new_img', function(req, res){
   // Send image url to frontend via io
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });

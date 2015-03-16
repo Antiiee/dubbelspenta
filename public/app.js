a01a182d20550625544e897b3185313fa59f0786
danielronnkvist <daniel@ronnkvist.com>
2015-03-16 20:35:00 +0100

    Removed old jquery

--- public/app.js
+++ public/app.js
@@ -2,14 +2,6 @@ var app = angular.module('ds', []);
 
 app.controller('photos', ['$scope', '$http', function($scope, $http){
   var socket = io();
-  $.ajax({
-            url: url,
-            type: 'POST',
-            crossDomain: true,
-            dataType: 'jsonp'
-        }).done(function (data) {
-            self.renderTemplate(data);
-        });
   socket.on('image', function(image){
     console.log(image);
     $http({

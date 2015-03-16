ded6a65f5752f09ae674569452090fbbe407134b
danielronnkvist <daniel@ronnkvist.com>
2015-03-16 18:10:32 +0100

    Added websockets

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var ig = require('instagram-node').instagram(),
     fs = require('fs'),
     express = require('express'),
     app = express(),
-    ejs = require('ejs'),
-    http = require('http'),
-    io = require('socket.io')(http);
+    ejs = require('ejs');
 
 // read instagram info
 ig.use({ client_id: process.env.APP_ID,
@@ -16,20 +14,25 @@ app.set('port', process.env.PORT || 8000);
 app.use("/public",express.static(__dirname+"/public"));
 app.use(express.static('node_modules'));
 
+var server = app.listen(app.get('port'));
+var io = require('socket.io').listen(server);
+
 // Subscribe to hashtag
 ig.add_tag_subscription('dubbelspenta', 'http://dubbelspenta.herokuapp.com/new_img', function(err, result, remaining, limit){
-  console.log("Successfully subscribed to hashtag");
+  if(!err)
+    console.log("Successfully subscribed to hashtag");
 });
 
 app.get('/', function(req, res){
   res.render('index.html')
 })
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 // Bind all types of requests to '/new_img'
 app.all('/new_img', function(req, res){
   // Send image url to frontend via io
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  io.emit('image', req.data);
 });
